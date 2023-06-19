6535709aaaa6e6671dc33e65ddd6854b19928a74
sendkite <sendkite1@gmail.com>
2023-06-20 01:23:34 +0900

    feat attribute inject

--- src/main/java/com/example/helloservlet/expert001_01/Car.java
+++ src/main/java/com/example/helloservlet/expert001_01/Car.java
@@ -3,7 +3,11 @@ package com.example.helloservlet.expert001_01;
 public class Car {
     Tire tire;
 
-    public Car(Tire tire) {
+    public Tire getTire() {
+        return tire;
+    }
+
+    public void setTire(Tire tire) {
         this.tire = tire;
     }
 
--- src/main/java/com/example/helloservlet/expert001_01/Driver.java
+++ src/main/java/com/example/helloservlet/expert001_01/Driver.java
@@ -2,7 +2,8 @@ package com.example.helloservlet.expert001_01;
 
 public class Driver {
     public static void main(String[] args) {
-        Car car = new Car(new AmericaTire());
+        Car car = new Car();
+        car.setTire(new KoreaTire());
         System.out.println(car.getTireBrand());
     }
 }
