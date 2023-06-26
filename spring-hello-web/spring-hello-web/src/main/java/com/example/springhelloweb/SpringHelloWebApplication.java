00f17590a232bd58bd4ba829dad3349aac20f059
sendkite <sendkite1@gmail.com>
2023-06-26 21:41:40 +0900

    feat spring-web setting

---
+++ spring-hello-web/spring-hello-web/src/main/java/com/example/springhelloweb/SpringHelloWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhelloweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelloWebApplication.class, args);
+    }
+
+}
