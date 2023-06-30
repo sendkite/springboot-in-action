1ec66ce4f21bfe26a36f3869c01b29adab80412c
sendkite <sendkite1@gmail.com>
2023-07-01 00:09:11 +0900

    feat 조건에 따른 테스트 구성

--- todos/src/main/java/com/example/todos/data/TodoDataInitializer.java
+++ todos/src/main/java/com/example/todos/data/TodoDataInitializer.java
@@ -3,13 +3,21 @@ package com.example.todos.data;
 import com.example.todos.core.todos.domain.Todo;
 import com.example.todos.core.todos.domain.TodoRepository;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 /**
- * 빈 초기화 시점에 메서드 호출해서 데이터를 초기화한다.
+ * @see InitializingBean : 빈 초기화 시점에 실행
+ * @see ApplicationRunner : 스프링 부트가 실행된 이후에 실행
+ * @see CommandLineRunner : 스프링 부트가 실행된 이후에 실행
+ * @see ConditionalOnProperty : 조건에 따라 동작 설정
  */
 @Component
-public class TodoDataInitializer implements InitializingBean {
+@ConditionalOnProperty(name = "todo.data.initialize", havingValue = "true")
+public class TodoDataInitializer implements InitializingBean, ApplicationRunner, CommandLineRunner {
 
     private final TodoRepository todoRepository;
 
@@ -20,7 +28,15 @@ public class TodoDataInitializer implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         todoRepository.save(Todo.create("할일 1"));
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         todoRepository.save(Todo.create("할일 2"));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         todoRepository.save(Todo.create("할일 3"));
     }
 }
