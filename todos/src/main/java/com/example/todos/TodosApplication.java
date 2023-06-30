f7b9155f1441673125d3732ea977b61c0df490c2
sendkite <sendkite1@gmail.com>
2023-06-30 19:29:24 +0900

    feat setting static recourese

--- todos/src/main/java/com/example/todos/TodosApplication.java
+++ todos/src/main/java/com/example/todos/TodosApplication.java
@@ -1,19 +1,12 @@
 package com.example.todos;
 
-import com.example.todos.web.WebMvcConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodosApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfiguration todoappWebMvcConfiguration() {
-        return new WebMvcConfiguration();
     }
 }
--- todos/src/main/java/com/example/todos/web/WebMvcConfiguration.java
+++ todos/src/main/java/com/example/todos/web/WebMvcConfiguration.java
@@ -1,12 +1,28 @@
 package com.example.todos.web;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.slf4j.Logger;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public WebMvcConfiguration() {
+        logger.debug("스프링 MVC 설정자 생성");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 리소스 핸들러 등록
+//        registry.addResourceHandler("/assets/**")
+//                .addResourceLocations("");
+    }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
