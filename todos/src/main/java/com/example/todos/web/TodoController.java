b375441dfa6e91713b9fb63b6aaaab76dd8df0c2
sendkite <sendkite1@gmail.com>
2023-07-01 21:27:48 +0900

    feat csv downloader

--- todos/src/main/java/com/example/todos/web/TodoController.java
+++ todos/src/main/java/com/example/todos/web/TodoController.java
@@ -1,17 +1,23 @@
 package com.example.todos.web;
 
+import com.example.todos.core.todos.application.TodoFinder;
+import com.example.todos.core.todos.domain.Todo;
+import com.example.todos.web.convert.TodoSpreadsheetConverter;
 import com.example.todos.web.model.SiteProperties;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.AbstractView;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -19,9 +25,11 @@ import java.util.Map;
 public class TodoController {
 
     private final SiteProperties siteProperties;
+    private final TodoFinder finder;
 
-    public TodoController(SiteProperties siteProperties) {
+    public TodoController(SiteProperties siteProperties, TodoFinder finder) {
         this.siteProperties = siteProperties;
+        this.finder = finder;
     }
 
     @ModelAttribute("site")
@@ -34,11 +42,11 @@ public class TodoController {
     }
 
     @RequestMapping(value = "/todos", produces = "text/csv")
-    public void todosCsv() throws Exception {
-
+    public void todosCsv(Model model) throws Exception {
+        model.addAttribute("todos", new TodoSpreadsheetConverter().convert(finder.getAll()));
     }
 
-    public static class TodoViewResolver implements ViewResolver {
+    public static class TodoCsvViewResolver implements ViewResolver {
 
         @Override
         public View resolveViewName(String viewName, Locale locale) throws Exception {
@@ -56,8 +64,24 @@ public class TodoController {
         }
 
         @Override
+        protected boolean generatesDownloadContent() {
+            return true;
+        }
+
+        @Override
         protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
             logger.debug("model as csv");
+
+            response.addHeader("Content-Disposition", "attachment; filename=\"todos.csv\"");
+            response.getWriter().println("id,title,completed");
+
+            List<Todo> todos = (List<Todo>) model.getOrDefault("todos", Collections.emptyList());
+            for (Todo todo : todos) {
+                String line = String.format("%d,%s,%s", todo.getId(), todo.getTitle(), todo.isCompleted());
+                response.getWriter().println(line);
+            }
+
+            response.flushBuffer();
         }
     }
 }
--- todos/src/main/java/com/example/todos/web/WebMvcConfiguration.java
+++ todos/src/main/java/com/example/todos/web/WebMvcConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.todos.web;
 
+import com.example.todos.commons.web.view.CommaSeparatedValuesView;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-import org.slf4j.Logger;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -24,17 +25,29 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 //                .addResourceLocations("");
     }
 
+    @Bean(name = "todos")
+    public CommaSeparatedValuesView todoCsvView() {
+        return new CommaSeparatedValuesView();
+    }
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
-        // registry.enableContentNegotiation();
-        // 위와 같이 직접 설정하면, 스프링부트가 구성한 ContentNegotiatingViewResolver 전략이 무시된다.
-        registry.viewResolver(new TodoController.TodoViewResolver());
+//         registry.enableContentNegotiation(new CommaSeparatedValuesView());
+        // 위와 같이 직접 설정하면, 스프링부트가 구성한 ContentNegotiatingViewResolver 전략이 무시된다. 사용에 주의
+//        registry.viewResolver(new TodoController.TodoViewResolver());
     }
 
-    public static class ContentNegotiationCustomizer {
-
-        public void configurer(ContentNegotiatingViewResolver viewResolver) {
-            // TODO ContentNegotiatingViewResolver 사용자 정의
-        }
-    }
+//    @Configuration
+//    public static class ContentNegotiationCustomizer {
+        // todo : Autowired로 디폴트뷰 구성하면 rest api 동작안함
+//
+//        @Autowired
+//        public void configurer(ContentNegotiatingViewResolver viewResolver) {
+//            List<View> defaultViews = new ArrayList<>(viewResolver.getDefaultViews());
+//            defaultViews.add(new CommaSeparatedValuesView());
+//            defaultViews.add(new MappingJackson2JsonView());
+//
+//            viewResolver.setDefaultViews(defaultViews);
+//        }
+//    }
 }
